aba254622cb40ef0de75a376785dba1dabbb4472
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-08-07 22:01:53 +0530

    Practice set

--- src/CWH_35_Practice_Set.java
+++ src/CWH_35_Practice_Set.java
@@ -1,4 +1,12 @@
 public class CWH_35_Practice_Set {
+    static void multiplication(int n){
+
+        for (int i=0;i<=11;i++){
+            n = n * i;
+        }
+
+    }
+
     public static void main(String[] args) {
 
 
