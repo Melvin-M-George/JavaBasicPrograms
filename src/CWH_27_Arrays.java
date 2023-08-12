2e6f0a6a962d5e5a27a8b4680b7c135b1add0e60
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-08-13 02:37:11 +0530

    Arrays

---
+++ src/CWH_27_Arrays.java
@@ -0,0 +1,9 @@
+public class CWH_27_Arrays {
+    public static void main(String[] args) {
+        int [] marks = {99,98,97,96,95};
+        float [] marks1 ={99.0f,98.5f,97.5f,93,5f,94.0f};
+        String [] name = {"M","E","L","V","I","N"};
+        System.out.println(marks.length);
+
+    }
+}
