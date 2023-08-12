6ff91ba6b5463f45b9ebefc53d4d96437c4e6e8e
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-08-12 19:25:40 +0530

    Arrays

---
+++ src/CWH_26_arrays.java
@@ -0,0 +1,18 @@
+public class CWH_26_arrays {
+    public static void main(String[] args) {
+        /*
+        int [] marks = new int[5];
+        marks[0]=100;
+        marks[1]=99;
+        marks[2]=95;
+        marks[3]=97;
+        marks[4]=98;
+
+
+         */
+
+        int [] marks = {98,89,90,95,97};
+        System.out.println(marks[3]);
+
+    }
+}
