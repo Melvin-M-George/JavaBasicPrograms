29b5459d80e840cb7bae7d3d6129ed36f3ef3030
Melvin-M-George <melvinmgeorge1234@gmail.com>
2023-08-23 18:26:57 +0530

    Arrays Practice Set

---
+++ src/CWH_29_Practice_Set_6.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class CWH_29_Practice_Set_6 {
+    public static void main(String[] args) {
+        //Question-1
+        /*
+        float[] marks = {98.5f,95.5f,92.5f,97.0f,96.0f};
+
+        float sum =0;
+        for(int i =0;i< marks.length;i++){
+            sum = sum + marks[i];
+        }
+        System.out.printf("The sum of marks is %f",sum);
+
+
+        for(float element: marks){
+            sum = sum + element;
+        }
+        System.out.println(sum);
+
+         */
+
+        //Question-2
+        /*
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int [] marks = {99,98,97,96,95};
+        boolean isInArray = false;
+        for (int i=0;i< marks.length;i++){
+            if(num==marks[i]){
+                isInArray=true;
+            }
+        }
+        if(isInArray){
+            System.out.println("The integer is present in the array");
+        }
+        else {
+            System.out.println("The integer is not present in the array");
+        }
+
+         */
+        //Question-3
+
+
+
+
+
+    }
+}
