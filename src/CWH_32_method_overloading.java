48c448c8bcba21c6eb4f18d861c2de76f1aeb141
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-01 01:12:43 +0530

    Method Overloading

--- src/CWH_32_method_overloading.java
+++ src/CWH_32_method_overloading.java
@@ -1,4 +1,13 @@
 public class CWH_32_method_overloading {
+    static void foo(){
+        System.out.println("Good Morning");
+    }
+    static void foo(int a){
+        System.out.println("The time is about "+ a);
+    }
+    static void foo(int a, int b){
+        System.out.println("The time is "+ a+":"+b);
+    }
     static void change(int a){
         a = 98;
     }
@@ -7,11 +16,18 @@ public class CWH_32_method_overloading {
     }
 
     public static void main(String[] args) {
+
         //tellJoke();
-        int [] marks = {1,2,3,4,5};
-        int x = 45;
-        change(x);
-        System.out.println("The value of x after running change - " + x);
+        //int [] marks = {1,2,3,4,5};
+        //int x = 45;
+        //change(x);
+        //System.out.println("The value of x after running change = " + x);
+        foo();
+        foo(4);
+        foo(10,15);//Arguments are actual\
+
+
+
 
 
     }
