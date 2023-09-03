7fae9f50340f401b5aaa6df3140147c5b5190ac1
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-03 23:07:48 +0530

    Recursion

--- src/CWH_34_Recursion.java
+++ src/CWH_34_Recursion.java
@@ -1,8 +1,19 @@
 public class CWH_34_Recursion {
-    static int factorial(){
-
+    static int factorial(int n){
+//        if(n==0 || n==1){
+//            return 1;
+//        }
+//        else{
+//            return n*factorial(n-1);
+//        }
+        int factorial = 1;
+        for(int i=n;i>0;i--){
+            factorial = factorial * i;
+        }
+        return factorial;
     }
     public static void main(String[] args) {
+        System.out.println("Factorial of 5 is : "+factorial(5));
 
     }
 }
