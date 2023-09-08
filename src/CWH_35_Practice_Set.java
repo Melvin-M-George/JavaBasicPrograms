2215b8ab3403bb214949964d739bd806861b7060
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-08 12:40:22 +0530

    Practice set

--- src/CWH_35_Practice_Set.java
+++ src/CWH_35_Practice_Set.java
@@ -1,12 +1,9 @@
 public class CWH_35_Practice_Set {
     //Question -1
     static void multiplication_table(int n){
-
-        int multi = 1;
         System.out.println("The multiplication table of "+n+ " is");
         for(int i=1;i<11;i++){
-            multi = i * n;
-            System.out.printf("%d x %d = %d\n",n,i,multi);
+            System.out.printf("%d x %d = %d\n",n,i,n*i);
         }
     }
     public static void main(String[] args) {
