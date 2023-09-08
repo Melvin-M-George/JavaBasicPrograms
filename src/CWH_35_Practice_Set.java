9925b74953bc3248e44ea0b83ab39c436bd61185
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-08 20:39:40 +0530

    Practice set

--- src/CWH_35_Practice_Set.java
+++ src/CWH_35_Practice_Set.java
@@ -6,8 +6,65 @@ public class CWH_35_Practice_Set {
             System.out.printf("%d x %d = %d\n",n,i,n*i);
         }
     }
+    static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static int sum_of_natural_numbers(int n){
+        if(n==1){
+            return 1;
+        }else{
+            return n + sum_of_natural_numbers(n-1);
+
+        }
+    }
+    static void pattern2(int n){
+        for(int i=0;i<n;i++){
+            for(int j=n;j>=i+1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static int nth_fibonacci(int n){
+        if(n==1 ){
+            return 0;
+        } else if (n==2) {
+            return 1;
+        }
+        else{
+            return   nth_fibonacci(n-1) + nth_fibonacci(n-2);
+        }
+
+    }
+
     public static void main(String[] args) {
-        multiplication_table(10);
+        //Question -1
+        //multiplication_table(10);
+
+        //Question -2
+        //pattern1(5);
+
+
+        //int x = 10;
+        //System.out.println("The sum of first 10 natural numbers is " + sum_of_natural_numbers(x));
+
+        //Question-4
+        //pattern2(5);
+
+        //Question-5
+        int x = 6;
+        System.out.println("The 6th term in the fibonacci series is "+ nth_fibonacci(x));
+
+
+
+
 
 
     }
