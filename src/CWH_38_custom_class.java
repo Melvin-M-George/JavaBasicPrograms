6a64e72bb313e3281028cb8297203e7c4bb7c8dc
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-14 18:08:10 +0530

    Class and object

--- src/CWH_38_custom_class.java
+++ src/CWH_38_custom_class.java
@@ -1,14 +1,34 @@
 class Employee{
     int id;
+    int salary;
     String name;
+    public void printDetails(){
+        System.out.println("My id is " + id + " and my name is "+name);
+    }
+    public int getSalary(){
+        return salary;
+    }
 }
 public class CWH_38_custom_class {
     public static void main(String[] args) {
         Employee melvin = new Employee(); // Instantiating a new Employee object
-        melvin.id = 10931;
+        Employee manav = new Employee(); // Instantiating a new Employee object
+
+        //Setting attributes
+        melvin.id = 2001;
         melvin.name = "Melvin M George";
-        System.out.println(melvin.id);
-        System.out.println(melvin.name);
+        melvin.salary=50000;
+
+        manav.id = 2005;
+        manav.name = "Manav M George";
+        //Printing Attributes
+        //System.out.println(melvin.id);
+        //System.out.println(melvin.name);
+        melvin.printDetails();
+        manav.printDetails();
+
+        int salary = melvin.getSalary();
+        System.out.println(salary);
 
     }
 }
