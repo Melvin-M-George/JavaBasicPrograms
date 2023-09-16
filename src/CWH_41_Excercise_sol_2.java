16f33b8ae6b675d32492376994d5e703f5a67750
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-16 17:04:58 +0530

    Rock Paper Scissor game

---
+++ src/CWH_41_Excercise_sol_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
+public class CWH_41_Excercise_sol_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 0 for Rock, 1 for Paper and 2 for Scissor");
+        int UserInput = sc.nextInt();
+
+        Random random = new Random();
+        int ComputerInput = random.nextInt(3);
+
+        if(UserInput==ComputerInput){
+            System.out.println("Draw, Play again!");
+        }
+        else if (UserInput==0 && ComputerInput==2 || UserInput==1 && ComputerInput==0 ||
+                UserInput==2 && ComputerInput==1) {
+            System.out.println("You Win!!");
+        }
+        else{
+            System.out.println("Computer Win!!");
+        }
+
+
+    }
+
+}
