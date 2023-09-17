5047373c164435f1dd2406675e10f79a3cf9557e
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-17 13:06:34 +0530

    Constructors

--- src/CWH_42_Constructors.java
+++ src/CWH_42_Constructors.java
@@ -22,15 +22,15 @@ class MyMainEmployee {
 //        id = 49;
 //        name = "Your_name";
 //    }
-    public MyMainEmployee(String myName){
-        id = 49;
+    public MyMainEmployee(String myName, int myId){
+        id = myId;
         name = myName;
     }
 }
 
 public class CWH_42_Constructors {
     public static void main(String[] args) {
-        MyMainEmployee harry = new MyMainEmployee();
+        MyMainEmployee harry = new MyMainEmployee("CodeWithHarry",143);
         //harry.setName("CodeWithHarry");
         System.out.println(harry.getId());
         System.out.println(harry.getName());
