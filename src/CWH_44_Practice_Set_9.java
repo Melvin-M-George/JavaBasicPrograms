36732e67cdfb729f79ffab45711a3d0b5362cff3
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-19 20:19:14 +0530

    Practice set

--- src/CWH_44_Practice_Set_9.java
+++ src/CWH_44_Practice_Set_9.java
@@ -20,9 +20,11 @@ public class CWH_44_Practice_Set_9 {
 
         Cylinder myCylinder = new Cylinder();
         myCylinder.setRadius(5);
-        System.out.println(myCylinder.getRadius());
+        int r = myCylinder.getRadius();
+        System.out.println(r);
         myCylinder.setHeight(10);
-        System.out.println(myCylinder.getHeight());
+        int h = myCylinder.getHeight();
+        System.out.println(h);
 
 
     }
