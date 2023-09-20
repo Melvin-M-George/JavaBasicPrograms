abf43dcf0c04b8c8471a10f2c478b134c67ac8ce
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-20 18:59:24 +0530

    Inheritance

---
+++ src/CWH_45_Inheritance.java
@@ -0,0 +1,30 @@
+
+class Base {
+    int x;
+
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void printMe(){
+        System.out.println("Hello World");
+    }
+}
+
+class Derived extends Base{
+    int y;
+    public int getY(){
+        return y;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+}
+
+public class CWH_45_Inheritance {
+    public static void main(String[] args) {
+
+    }
+}
