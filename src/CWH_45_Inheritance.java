d93f819b417569851e8aba258b2c9f38dcf39a02
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-21 15:53:41 +0530

    Inheritance

--- src/CWH_45_Inheritance.java
+++ src/CWH_45_Inheritance.java
@@ -22,17 +22,20 @@ class Derived extends Base{
         this.y = y;
     }
 }
-
 public class CWH_45_Inheritance {
     public static void main(String[] args) {
+        //Creating object of Base class
         Base b = new Derived();
         b.setX(4);
         System.out.println(b.getX());
 
         //Creating an object of Derived class and accessing methods of Base class
         Derived d = new Derived();
-        b.setX(4);
-        System.out.println(b.getX());
+        d.setX(43);
+        System.out.println(d.getX());
+        d.setY(21);
+        System.out.println(d.getY());
+
 
 
     }
