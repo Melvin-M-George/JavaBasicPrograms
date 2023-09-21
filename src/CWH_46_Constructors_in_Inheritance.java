2268c818aee2bc3a2efa691dcb3554c2016af355
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-22 01:45:42 +0530

    Inheritance and Constructors

--- src/CWH_46_Constructors_in_Inheritance.java
+++ src/CWH_46_Constructors_in_Inheritance.java
@@ -2,16 +2,23 @@ class Base1 {
     Base1(){
         System.out.println("This is the constructor of Base1 Class");
     }
+    Base1(int a ){
+        System.out.println("This is an overloaded constructor of Base1 Class with value:"+a);
+    }
+    Base1(int a ,int b){
+        System.out.println("This is an overloaded constructor of Base1 Class with value:"+a*b);
+    }
 }
 
 class Derived1 extends Base1{
     Derived1(){
+        super(2,4);
         System.out.println("This is the constructor of Derived1 Class");
     }
 }
 public class CWH_46_Constructors_in_Inheritance{
     public static void main(String[] args) {
-        Base1 b = new Base1();
+        //Base1 b = new Base1();
         Derived1 d = new Derived1();
     }
 }
