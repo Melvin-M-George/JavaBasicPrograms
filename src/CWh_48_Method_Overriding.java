6210ee72e0e4ea5214c665dabe7f738e813f1c3b
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-24 20:01:46 +0530

    Method_Overriding

--- src/CWh_48_Method_Overriding.java
+++ src/CWh_48_Method_Overriding.java
@@ -8,6 +8,10 @@ class A{
     }
 }
 class B extends A{
+    @Override
+    public void meth2(){
+        System.out.println("This is method 2 of class B");
+    }
     public void meth3(){
         System.out.println("This is method 3 of class B");
     }
@@ -16,6 +20,9 @@ public class CWh_48_Method_Overriding {
     public static void main(String[] args) {
         A a = new A();
         a.meth2();
+
+        B b = new B();
+        b.meth2();
 
     }
 }
